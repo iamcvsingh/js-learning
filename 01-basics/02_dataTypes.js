67589eb0c4d92f1c3cb955b86891fb659940bb33
CHANDRAVIR SINGH PARIHAR <10529007+iamcvsingh@users.noreply.github.com>
2026-03-18 09:19:05 +0000

    datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+let nameodStd = "Chandravir"
+let age = 35
+let isLoggedIn = false
+let mobileNo = null
+let emailId
+
+const user = {
+    name: "Chandravir",
+    age: 35
+}
+
+// number => 2 to pwr 53 //	A number representing a numeric value
+// string => "" // A text of characters enclosed in quotes
+// boolean => true/false // A data type representing true or false
+// null => object // A value representing object absence
+// undefined => undefined  // A variable with no assigned value
+// symbol => A unique primitive identifier
+
+console.log(typeof nameodStd);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof mobileNo);
+console.log(typeof emailId);
+console.log(typeof user);
+
